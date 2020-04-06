5e21d0e959b16f87165feaf49ba64de5ce5f95b1
mrTran <anhvanluc1999@gmail.com>
2020-04-06 22:57:13 +0700

    Add user list

--- index.js
+++ index.js
@@ -3,12 +3,21 @@ var app =  express();
 
 var port = 3000;
 
-app.get('/', function(request, response) {
-	response.send('<h1>Hello world</h1><a href="/users">User List</a>');
+app.set('view engine', 'pug');
+app.set('views', './views')
+app.get('/', function(req, res) {
+	res.render('index', {
+		name: 'Johnny Dang'
+	});
 });
-app.get('/users', function(request, response) {
-	response.send('Users list');
-})
+app.get('/users', function(req, res) {
+	res.render('users/index', {
+	users: [
+			{id: 1, name: 'tom'},
+			{id: 2, name: 'crab'}
+		]
+	});
+});
 app.listen(port, function() {
 	console.log('Server is listening to port ' + port);
 });
