c35f8cf7ecb7594d226a1a6506b2c0dfd03a4fb3
mrTran <anhvanluc1999@gmail.com>
2020-04-07 09:55:57 +0700

    Seach query

--- index.js
+++ index.js
@@ -10,12 +10,23 @@ app.get('/', function(req, res) {
 		name: 'Johnny Dang'
 	});
 });
+var users = [
+	{id: 1, name: 'tom'},
+	{id: 2, name: 'crab'}
+];
 app.get('/users', function(req, res) {
 	res.render('users/index', {
-	users: [
-			{id: 1, name: 'tom'},
-			{id: 2, name: 'crab'}
-		]
+		users: users
+	});
+});
+app.get('/users/search', function(req, res ) {
+	var q = req.query.q;
+	var matchUsers = users.filter(function(user) {
+		return user.name.indexOf(q) !== -1;
+	});
+
+	res.render('users/index', {
+		users: matchUsers
 	});
 });
 app.listen(port, function() {
