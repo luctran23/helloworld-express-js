e5c5eb06f4e682d167741afb5759a1a1bb2eb00f
mrTran <anhvanluc1999@gmail.com>
2020-04-07 21:45:07 +0700

    Create users method

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var express = require('express');
+var bodyParsers = require('body-parser');
 var app =  express();
 
 var port = 3000;
 
+
 app.set('view engine', 'pug');
-app.set('views', './views')
+app.set('views', './views');
+
+app.use(bodyParsers.json());
+app.use(bodyParsers.urlencoded({extended: true }));
 app.get('/', function(req, res) {
 	res.render('index', {
 		name: 'Johnny Dang'
@@ -28,6 +33,15 @@ app.get('/users/search', function(req, res ) {
 	res.render('users/index', {
 		users: matchUsers
 	});
+});
+
+app.get('/users/create', function(req, res) {
+	res.render('users/create');
+});
+
+app.post('/users/create', function(req, res) {
+	users.push(req.body);
+	res.redirect('/users');
 });
 app.listen(port, function() {
 	console.log('Server is listening to port ' + port);
